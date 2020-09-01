7a5a54acc4432f3ea7395d65947c89cd0000f843
shintumintu <mintushintu41@gmail.com>
2020-09-01 09:30:57 +0530

    first commit

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,1 +1,9 @@
+import 'package:Scaleindia/Services/navigation_service.dart';
 import 'package:get_it/get_it.dart';
+
+GetIt locator = GetIt.instance;
+
+void setUpLocator() {
+  locator.registerLazySingleton(() => NavigationService());
+  locator.registerLazySingleton(() => DialogService());
+}
