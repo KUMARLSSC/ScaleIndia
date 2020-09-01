0e2e6993559491b235caab3751bbd83895204abf
shintumintu <mintushintu41@gmail.com>
2020-09-01 13:33:14 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'Manager/dialog_manager.dart';
 import 'Models/router.dart';
 import 'Pages/splash_page.dart';
