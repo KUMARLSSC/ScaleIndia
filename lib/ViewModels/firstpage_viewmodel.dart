f3773030bed3280a8d2b6c5bee6693f64f707eb3
shintumintu <mintushintu41@gmail.com>
2020-09-08 10:53:00 +0530

    first commit

--- lib/ViewModels/firstpage_viewmodel.dart
+++ lib/ViewModels/firstpage_viewmodel.dart
@@ -1,17 +1,34 @@
-import 'package:Scaleindia/Models/route_names.dart';
+import 'dart:convert';
 import 'package:Scaleindia/Services/dialog_service.dart';
 import 'package:Scaleindia/Services/navigation_service.dart';
-import 'package:flutter/foundation.dart';
+import 'package:Scaleindia/json_api.dart';
+import 'package:http/http.dart' as http;
 
 import '../locator.dart';
 import 'base_model.dart';
 
 class FirstPageViewModel extends BaseModel {
+  // ignore: unused_field
   final NavigationService _navigationService = locator<NavigationService>();
 
-   final DialogService _dialogService = locator<DialogService>();
+  // ignore: unused_field
+  final DialogService _dialogService = locator<DialogService>();
+  String assessrName = '';
+  String center = '';
+  String batchNo = 0;
+  String url = 'http://www.json-generator.com/api/json/get/cgtsezzKKW?indent=2';
 
-  Future firstpage({@required String requestid ,})async{
+  Future getData(String id) async {
+    String profile = url + id;
+    var response =
+        await http.get(profile, headers: {"Accept": "application/json"});
+     var resBody = json.decode(response.body);
+     assessrName = resBody['assessrName'];
+    batchNo = resBody['batchNo'];
+    center = resBody['center'];
+  }
+
+  /*Future firstpage({@required String requestid ,})async{
      if (requestid.isEmpty){
          await _dialogService.showDialog(
           title: 'Given field is empty ',
@@ -24,5 +41,5 @@ class FirstPageViewModel extends BaseModel {
 
   void navigateToSecondPage() {
     _navigationService.navigateTo(SecondViewRoute);
-  }
+  }*/
 }
