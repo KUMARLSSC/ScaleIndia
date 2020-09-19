d34625a02d4a03251efd3c27373007ded92145f3
shintumintu <mintushintu41@gmail.com>
2020-09-19 11:27:29 +0530

    first commit

--- lib/ViewModels/thirdpage_viewmodel.dart
+++ lib/ViewModels/thirdpage_viewmodel.dart
@@ -1,7 +1,6 @@
 import 'package:Scaleindia/ApiModel/candidate_api.dart';
 import 'package:Scaleindia/Models/route_names.dart';
 import 'package:Scaleindia/Services/candidate_service.dart';
-import 'package:Scaleindia/Services/dialog_service.dart';
 import 'package:Scaleindia/Services/navigation_service.dart';
 import '../locator.dart';
 import 'base_model.dart';
@@ -9,22 +8,13 @@ import 'base_model.dart';
 class ThirdPageViewModel extends BaseModel {
   final NavigationService _navigationService = locator<NavigationService>();
   final CandidateService _candidateService = locator<CandidateService>();
-  final DialogService _dialogService = locator<DialogService>();
 
   List<Candidate> get posts => _candidateService.candidate;
 
   Future getPosts(int resId) async {
     setBusy(false);
-    var result = await _candidateService.getPostsForUser(resId);
+    await _candidateService.getPostsForUser(resId);
     setBusy(true);
-    if (result) {
-      _navigationService.navigateTo(FourthViewRoute);
-    } else {
-   await _dialogService.showDialog(
-        title:'Server Error',
-        description: result,
-      );
-    }
   }
 
   void navigateToFourthPage() {
