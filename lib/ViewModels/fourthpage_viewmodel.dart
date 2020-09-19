816129972af2625e1173c71fac1b64216a161b24
shintumintu <mintushintu41@gmail.com>
2020-09-19 12:29:37 +0530

    first commit

--- lib/ViewModels/fourthpage_viewmodel.dart
+++ lib/ViewModels/fourthpage_viewmodel.dart
@@ -1,21 +1,11 @@
-import 'package:Scaleindia/ApiModel/candidate_api.dart';
 import 'package:Scaleindia/Models/route_names.dart';
-import 'package:Scaleindia/Services/candidate_service.dart';
 import 'package:Scaleindia/Services/navigation_service.dart';
 import '../locator.dart';
 import 'base_model.dart';
 
 class FourthPageViewModel extends BaseModel {
   final NavigationService _navigationService = locator<NavigationService>();
-final CandidateService _candidateService = locator<CandidateService>();
 
-  List<Candidate> get posts => _candidateService.candidate;
-
-  Future getPosts(int resId) async {
-    setBusy(false);
-    await _candidateService.getPostsForUser(resId);
-    setBusy(true);
-  }
   
   void navigateToFifthPage() {
     _navigationService.navigateTo(FifthViewRoute);
