12ee4e3f97ec6dfa494768350ee18e28944365ff
shintumintu <mintushintu41@gmail.com>
2020-09-24 17:32:08 +0530

    first commit

--- lib/Pages/fourth_page.dart
+++ lib/Pages/fourth_page.dart
@@ -13,96 +13,103 @@ class FourthPage extends StatelessWidget {
     CenterAssesor centerAssesor = Provider.of<CenterAssesor>(context);
     return ViewModelBuilder<FourthPageViewModel>.reactive(
       onModelReady: (model) => model.getPosts(centerAssesor.asId),
-        viewModelBuilder: () => FourthPageViewModel(),
-        builder: (context,model ,child ) => Scaffold(
-      appBar: PreferredSize(
-              child: header(context, isAppTitle: true, isIcon: false),
-              preferredSize: Size.fromHeight(50.0)),
-              body: Center(
-                child:Container(
-              height: MediaQuery.of(context).size.height - 30.0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width - 5.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        colors: [
-                          Colors.blue[900],
-                          Colors.blue[800],
-                          Colors.blue[400]
-                        ]),
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
+      viewModelBuilder: () => FourthPageViewModel(),
+      builder: (context, model, child) => Scaffold(
+        appBar: PreferredSize(
+            child: header(context, isAppTitle: true, isIcon: false),
+            preferredSize: Size.fromHeight(50.0)),
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height - 30.0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width - 5.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                  gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+                    Colors.blue[900],
+                    Colors.blue[800],
+                    Colors.blue[400]
+                  ]),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
                         padding: EdgeInsets.all(7),
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Center(child: Text('Participant List', style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 24.0,
-                                fontWeight: FontWeight.bold,
-                              ),),),
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(
+                                child: Text(
+                                  'Participant List',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
                               Divider(
-                              color: Colors.black38,
-                              height: 15,
-                            ),
-                           Row(
-                             mainAxisAlignment: MainAxisAlignment.spaceBetween ,
-                                children: [ 
-                                   Text(
-                                  'ID',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20.0,
+                                color: Colors.black38,
+                                height: 15,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'ID',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  'Name',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                    fontSize: 20.0,
+                                  Text(
+                                    'Name',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 20.0,
+                                    ),
                                   ),
-                                ),
-                                 Text(
-                                  'Exam',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                    fontSize: 20.0,
+                                  Text(
+                                    'Exam',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 20.0,
+                                    ),
                                   ),
-                                ),
                                 ],
                               ),
-                              SizedBox(height: 10,),
+                              SizedBox(
+                                height: 10,
+                              ),
                               Container(
-                                height:MediaQuery.of(context).size.height - 184.0,
-                                child:model.busy == false?Center(
-                child: CircularProgressIndicator(),
-              ):_getPostUi(model.posts)
-                              )
-                             
-                               ]
-                          )),
-                    ],
-                  ),
+                                  height: MediaQuery.of(context).size.height -
+                                      184.0,
+                                  child: model.busy == false
+                                      ? Center(
+                                          child: CircularProgressIndicator(),
+                                        )
+                                      : _getPostUi(model.posts))
+                            ])),
+                  ],
                 ),
               ),
-            ), 
-                ),
-    ),
-        
-        );
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _getPostUi(List<Candidate> posts) => ListView.separated(
-    separatorBuilder: (context, index) => Divider(color: Colors.black,),
+        separatorBuilder: (context, index) => Divider(
+          color: Colors.black,
+        ),
         itemCount: posts.length,
         itemBuilder: (BuildContext context, int index) => ParticipantWidget(
           candidate: posts[index],
