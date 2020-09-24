84bc5e623139530df794eef234c65fb1e8fd840f
shintumintu <mintushintu41@gmail.com>
2020-09-24 17:26:43 +0530

    first commit

--- lib/widgets/participant_widget.dart
+++ lib/widgets/participant_widget.dart
@@ -1,8 +1,6 @@
 import 'package:Scaleindia/ApiModel/candidate_api.dart';
-import 'package:Scaleindia/List/participant_list.dart';
-import 'package:Scaleindia/ViewModels/fourthpage_viewmodel.dart';
+
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
 
 class ParticipantWidget extends StatelessWidget {
   final Candidate candidate;
@@ -11,50 +9,66 @@ class ParticipantWidget extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return  Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            candidate.clEnrollmentNo,
-            maxLines: 2,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.normal,
-              fontSize: 16.0,
-            ),
+    final  theory = true;
+    final practical = true;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          candidate.clEnrollmentNo,
+          maxLines: 2,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.normal,
+            fontSize: 16.0,
           ),
-          Text(
-            candidate.clName,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.normal,
-              fontSize: 15.0,
-            ),
+        ),
+        Text(
+          candidate.clName,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.normal,
+            fontSize: 15.0,
           ),
-          Container(
-            height: 30,
-            width: 80,
-            child: RaisedButton(
-              splashColor: Colors.blue,
-              elevation: 3.0,
-              color: new Color(0xffffffff),
-              child: Text(
-                'Start',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.blueAccent,
-                ),
-              ),
-              onPressed: () {
-              
-              },
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0),
-                
-              ),
-            ),
-          )
-        ],
+        ),
+        Container(
+          height: 30,
+          width: 80,
+          child: (candidate.clPracticalDone &&candidate.clTheoryDeone) ==(practical==theory)
+              ? RaisedButton(
+                  splashColor: Colors.blue,
+                  elevation: 3.0,
+                  color: Colors.black12,
+                  child: Text(
+                    'Completed',
+                    style: TextStyle(
+                      fontSize: 9.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                )
+              : RaisedButton(
+                  splashColor: Colors.blue,
+                  elevation: 3.0,
+                  color: new Color(0xffffffff),
+                  child: Text(
+                    'Start',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                )
+        )
+      ],
     );
   }
 }
