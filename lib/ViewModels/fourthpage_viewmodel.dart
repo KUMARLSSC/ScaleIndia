1b6e4f1bc3e8a0168666f6b9322d94073daf3e68
shintumintu <mintushintu41@gmail.com>
2020-09-25 09:59:41 +0530

    first commit

--- lib/ViewModels/fourthpage_viewmodel.dart
+++ lib/ViewModels/fourthpage_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:Scaleindia/ApiModel/candidate_api.dart';
 import 'package:Scaleindia/Models/route_names.dart';
 import 'package:Scaleindia/Services/candidate_service.dart';
+import 'package:Scaleindia/Services/dialog_service.dart';
 import 'package:Scaleindia/Services/navigation_service.dart';
 import '../locator.dart';
 import 'base_model.dart';
@@ -8,7 +9,7 @@ import 'base_model.dart';
 class FourthPageViewModel extends BaseModel {
   final NavigationService _navigationService = locator<NavigationService>();
  final CandidateService _candidateService = locator<CandidateService>();
-
+final DialogService _dialogService = locator<DialogService>();
   List<Candidate> get posts => _candidateService.candidate;
 
   Future getPosts(int resId) async {
@@ -19,5 +20,11 @@ class FourthPageViewModel extends BaseModel {
   
   void navigateToFifthPage() {
     _navigationService.navigateTo(FifthViewRoute);
+  }
+  void navigateToCompleted() {
+     _dialogService.showDialog(
+        title: 'Completed',
+        description: 'This candidate completed the theory and practical exams',
+      );
   }
 }
--- lib/widgets/participant_widget.dart
+++ lib/widgets/participant_widget.dart
@@ -1,6 +1,8 @@
 import 'package:Scaleindia/ApiModel/candidate_api.dart';
+import 'package:Scaleindia/ViewModels/fourthpage_viewmodel.dart';
 
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 
 class ParticipantWidget extends StatelessWidget {
   final Candidate candidate;
@@ -9,66 +11,73 @@ class ParticipantWidget extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    final  theory = true;
+    final theory = true;
     final practical = true;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          candidate.clEnrollmentNo,
-          maxLines: 2,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.normal,
-            fontSize: 16.0,
+    return ViewModelBuilder<FourthPageViewModel>.reactive(
+      viewModelBuilder: () => FourthPageViewModel(),
+      builder: (context, model, child) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            candidate.clEnrollmentNo,
+            maxLines: 2,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.normal,
+              fontSize: 16.0,
+            ),
           ),
-        ),
-        Text(
-          candidate.clName,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.normal,
-            fontSize: 15.0,
+          Text(
+            candidate.clName,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.normal,
+              fontSize: 15.0,
+            ),
           ),
-        ),
-        Container(
-          height: 30,
-          width: 80,
-          child: (candidate.clPracticalDone &&candidate.clTheoryDeone) ==(practical==theory)
-              ? RaisedButton(
-                  splashColor: Colors.blue,
-                  elevation: 3.0,
-                  color: Colors.black12,
-                  child: Text(
-                    'Completed',
-                    style: TextStyle(
-                      fontSize: 9.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                )
-              : RaisedButton(
-                  splashColor: Colors.blue,
-                  elevation: 3.0,
-                  color: new Color(0xffffffff),
-                  child: Text(
-                    'Start',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Colors.blueAccent,
-                    ),
-                  ),
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                )
-        )
-      ],
+          Container(
+              height: 30,
+              width: 80,
+              child: (candidate.clPracticalDone && candidate.clTheoryDeone) ==
+                      (practical == theory)
+                  ? RaisedButton(
+                      splashColor: Colors.blue,
+                      elevation: 3.0,
+                      color: Colors.black12,
+                      child: Text(
+                        'Completed',
+                        style: TextStyle(
+                          fontSize: 9.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        model.navigateToCompleted();
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    )
+                  : RaisedButton(
+                      splashColor: Colors.blue,
+                      elevation: 3.0,
+                      color: new Color(0xffffffff),
+                      child: Text(
+                        'Start',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      onPressed: () {
+                        model.navigateToFifthPage();
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ))
+        ],
+      ),
     );
   }
 }
