467495bdbca9d8c1dc49ea3722a5a9acafd37fb0
shintumintu <mintushintu41@gmail.com>
2020-10-10 01:10:57 +0530

    first commit

--- lib/Pages/EmployeeDashBoard/home_Page.dart
+++ lib/Pages/EmployeeDashBoard/home_Page.dart
@@ -1,8 +1,78 @@
+import 'package:Scaleindia/widgets/loader_animation.dart';
+import 'package:Scaleindia/widgets/style_constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("HomePage"));
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('Blog').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              Center(
+                child: ColorLoader3(),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot myblog = snapshot.data.documents[index];
+                    return Stack(
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 350,
+                          child: SingleChildScrollView(
+                            child: Padding(
+                            padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                            child: Material(
+                              color: Colors.white,
+                              elevation: 14.0,
+                              shadowColor: Color(0x802196f3),
+                              child: Center(
+                                child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        height: 200.0,
+                                        child: Image.network(
+                                          '${myblog['image']}',
+                                          fit: BoxFit.fill,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        '${myblog['title']}',
+                                        style: kTitleStyle,
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text(
+                                        '${myblog['subtitle']}',
+                                        style: kSubtitleStyle,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          )
+                        )
+                      ],
+                    );
+                  });
+            }
+            return ColorLoader3();
+          }),
+    );
   }
 }
