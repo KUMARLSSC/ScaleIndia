e5ddb9dd75991afe09a08bbdfab1a76dc8c8df57
shintumintu <mintushintu41@gmail.com>
2020-10-10 02:04:45 +0530

    first commit

--- lib/ViewModels/splashpage_viewmodel.dart
+++ lib/ViewModels/splashpage_viewmodel.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:Scaleindia/Models/route_names.dart';
 import 'package:Scaleindia/Services/navigation_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../locator.dart';
 import 'base_model.dart';
@@ -10,9 +11,18 @@ class SplashPageViewModel extends BaseModel {
   final NavigationService _navigationService = locator<NavigationService>();
 
   Future handleStartUpLogic() async {
-    Future.delayed(
-      Duration(seconds: 3),
-      () => _navigationService.navigateTo(OnBoardingPageViewRoute)
-    );
+    Future.delayed(Duration(seconds: 3),
+        () => _navigationService.navigateTo(OnBoardingPageViewRoute));
+  }
+
+  Future onBoardLogic() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+    if (_seen) {
+      _navigationService.navigateTo(OnBoardingPageViewRoute);
+    } else {
+      prefs.setBool('seen', true);
+     _navigationService.navigateTo(HomeViewRoute);
+    }
   }
 }
