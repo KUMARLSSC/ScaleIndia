299e666d6101202850e3d4bc5442ebb3c1024c67
shintumintu <mintushintu41@gmail.com>
2020-10-13 18:08:12 +0530

    first commit

--- lib/Pages/EmployerDashBoard/viewJob_page.dart
+++ lib/Pages/EmployerDashBoard/viewJob_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class ViewJobPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       resizeToAvoidBottomPadding: false,
       resizeToAvoidBottomInset: false,
       body: Container(
@@ -32,29 +32,29 @@ class ViewJobPage extends StatelessWidget {
                 ],
               ),
             ),
-           Container(
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 255, 255, 15),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(35),
-                        topRight: Radius.circular(35))),
-                child: Padding(
-                    padding: EdgeInsets.all(1),
-                    child: Column(
-                      children: <Widget>[
-                        Center(
-                            child: Text(
-                          "Naps Registration",
-                          style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-                        )),
-                       SizedBox(height: 5,),
-                        NapsRegisterWidget(),
-                      
-                        new Padding(padding: EdgeInsets.only(bottom: 10.0)),
-                      ],
+            Container(
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 15),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                      topRight: Radius.circular(35))),
+              child: Padding(
+                padding: EdgeInsets.all(1),
+                child: Column(
+                  children: <Widget>[
+                    Center(
+                        child: Text(
+                      "Naps Registration",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )),
+                    SizedBox(
+                      height: 5,
                     ),
-
-                
+                    NapsRegisterWidget(),
+                    new Padding(padding: EdgeInsets.only(bottom: 10.0)),
+                  ],
+                ),
               ),
             )
           ],
