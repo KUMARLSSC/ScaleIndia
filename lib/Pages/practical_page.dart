804cc6f32132866b66a5ee1ca206a91923d6d6dc
shintumintu <mintushintu41@gmail.com>
2020-10-20 17:35:57 +0530

    first commit

--- lib/Pages/practical_page.dart
+++ lib/Pages/practical_page.dart
@@ -1,22 +1,25 @@
 import 'package:Scaleindia/ApiModel/center_api.dart';
-import 'package:Scaleindia/ApiModel/practical_api.dart';
 import 'package:Scaleindia/ViewModels/practicalpage_viewmodel.dart';
 import 'package:Scaleindia/widgets/HeaderWidget.dart';
 import 'package:Scaleindia/widgets/practicalpage_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stacked/stacked.dart';
-class PracticalPage extends StatelessWidget {
-  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
-  final Practical practical;
-  PracticalPage({this.practical});
+class PracticalPage extends StatefulWidget {
+  @override
+  _PracticalPageState createState() => _PracticalPageState();
+}
+
+class _PracticalPageState extends State<PracticalPage> {
+final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+  
   @override
   Widget build(BuildContext context) {
     CenterAssesor centerAssesor = Provider.of<CenterAssesor>(context);
     return ViewModelBuilder<PracticalPageViewModel>.reactive(
       onModelReady: (model) => model.getPractical(centerAssesor.asId),
       viewModelBuilder: () => PracticalPageViewModel(),
-      builder: (context, model, child) => Scaffold(
+      builder: (context, model, child) => WillPopScope(child: Scaffold(
         resizeToAvoidBottomPadding: false,
         resizeToAvoidBottomInset: false,
         key: _key,
@@ -46,7 +49,8 @@ class PracticalPage extends StatelessWidget {
             ),
           ),
         ),
-      ),
+      ), 
+      onWillPop: _onWillPop)
     );
   }
   /*Widget _getPostUi(List<Practical> practical) => ListView.separated(
@@ -58,4 +62,29 @@ class PracticalPage extends StatelessWidget {
             practical: practical[index],
             ),
       );*/
+      Future<bool> _onWillPop() async {
+    return showDialog<bool>(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            content: Text(
+                "Are you sure you want to quit the Exam? All your progress will be lost."),
+            title: Text("Warning!"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Yes"),
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+              ),
+              FlatButton(
+                child: Text("No"),
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+              ),
+            ],
+          );
+        });
+  }
 }
